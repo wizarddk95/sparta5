00e7ee9237598fd99960ea2ded2ea089a70e13d1
wizarddk95 <102998769+wizarddk95@users.noreply.github.com>
2025-01-20 18:49:01 +0900

    Update using_api.py

--- using_api.py
+++ using_api.py
@@ -8,7 +8,7 @@ series_ids = {
     "Treasury Securities": "TREAST", 
     "Loans": "WLCFLL",
     "Reserve Balances with Fed": "WRBWFRBL",
-    "Deposits with F.R Bank": "WDTGAL",
+    "Deposits with F_R Bank": "WDTGAL",
     "Reverse Repurchase Agreements": "WLRRAL"
 }
 
